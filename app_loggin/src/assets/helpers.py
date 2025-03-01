687cb89f3675f5198ed714fd0a17df1e425ebcad
OyasumiDave-Gabriel <gafloresgutierrez@gmail.com>
2025-02-28 20:22:47 -0600

    Primer Commit

---
+++ app_loggin/src/assets/helpers.py
@@ -0,0 +1,11 @@
+import base64
+
+def convert_image_to_base64(image_path):
+    try:
+        with open(image_path, "rb") as image_file:
+            # Codificar la imagen en base64
+            base64_image = base64.b64encode(image_file.read()).decode('utf-8')
+            return base64_image
+    except Exception as e:
+        # Corregir la sintaxis del return
+        return f'Error: {str(e)}'
